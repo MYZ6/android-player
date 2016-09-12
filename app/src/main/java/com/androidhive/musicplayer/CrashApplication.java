01c23757b7a4371f4560ec14df6cffa730081604
lyz19890927 <lyz19890927@163.com>
2016-09-12 18:15:19 +0800

    crash handle

---
+++ app/src/main/java/com/androidhive/musicplayer/CrashApplication.java
@@ -0,0 +1,17 @@
+package com.androidhive.musicplayer;
+
+/**
+ * Created by liyzh on 2016/9/12.
+ */
+
+import android.app.Application;
+
+public class CrashApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CrashHandler crashHandler = CrashHandler.getInstance();
+        crashHandler.init(getApplicationContext());
+    }
+}
+
